f783b50706b36245e1e19fc33cfde4b285e57a28
tejqliky <tejqlik@gmail.com>
2025-10-13 11:45:01 -0400

    feat: updated REACT

--- frontend/repmeta-ui/src/App.tsx
+++ frontend/repmeta-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 /** Types */
 type Customer = { customer_id: number; customer_name: string };
